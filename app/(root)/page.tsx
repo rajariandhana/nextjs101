91fdde4d14f0262cd27e958dcbd0adeac15c0cc0
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2024-12-23 02:48:47 +0700

    hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,21 @@ import React from 'react'
 const page = () => {
   return (
     <>
-        <h1 className='text-2xl'>Home</h1>
+        <section className='pink_container'>
+          <h1 className='heading'>Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+          <p className='sub-heading !max-w-3xl'>
+            Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+          </p>
+        </section>
     </>
   )
 }
 
 export default page
+// export default function Home() {
+//   return (
+//     <>
+//         <h1 className='heading'>Home</h1>
+//     </>
+//   )
+// }
